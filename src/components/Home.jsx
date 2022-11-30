b40a1dd0b08ca78abfab508ee7edf9d8ecc76de0
Joshua Carroll <carrolljosh1976@gmail.com>
2022-11-29 20:24:43 -0500

    Added profile pic, LinkedIn and Github links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,10 @@ function Home() {
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <p className='text-[#FBEC5D]'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Joshua Carroll</h1>
+            <div className='flex justify-between'>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
+            <img className='w-24 md:w-36 lg:w-48 rounded-full' src='https://pbs.twimg.com/profile_images/1586786350690443264/srf6Kdg4_400x400.jpg' alt='profile'/>
+            </div>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>I am a recent graduate from a coding bootcamp at Grand Circus in Detroit,MI.
                 I enjoy building responsive full stack web applications.
             </p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -47,10 +47,10 @@ function Navbar() {
             <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
                 <ul>
                     <li className='w-[160px] h-[60px] flex justify-between items-center font-bold ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-                        <a className='flex justify-between items-center w-full text-gray-300' href="/">LinkedIn <FaLinkedin size={30} /></a>
+                        <a className='flex justify-between items-center w-full text-gray-300' href="https://www.linkedin.com/in/joshuajcarroll1976/">LinkedIn <FaLinkedin size={30} /></a>
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center font-bold ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-                        <a className='flex justify-between items-center w-full text-gray-300' href="/">Github <FaGithub size={30} /></a>
+                        <a className='flex justify-between items-center w-full text-gray-300' href="https://github.com/jcarroll1976">Github <FaGithub size={30} /></a>
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center font-bold ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
                         <a className='flex justify-between items-center w-full text-gray-300' href="/">Email <HiOutlineMail size={30} /></a>
